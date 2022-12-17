f31e31e1bb32e73c6019df535392e4430658facb
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-12-17 12:42:02 +0900

    update day9 part1

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -11,100 +11,129 @@ const stringifyCoord = (c: Coord) => {
   return `${c[0]}, ${c[1]}`;
 };
 
-let h: Coord = [0, 0],
-  t: Coord = [0, 0];
-
-const res = new Set<string>([stringifyCoord(t)]);
-
-const isTouching = () => {
+const isTouching = (h: Coord, t: Coord) => {
   const dx = Math.abs(h[0] - t[0]);
   const dy = Math.abs(h[1] - t[1]);
   return dx >= 0 && dx <= 1 && dy >= 0 && dy <= 1;
 };
 
-const isSameY = () => {
+const isSameY = (h: Coord, t: Coord) => {
   return h[1] === t[1];
 };
 
-const isSameX = () => {
+const isSameX = (h: Coord, t: Coord) => {
   return h[0] === t[0];
 };
 
-const moveUp = () => {
-  h[1] += 1;
-  if (isTouching()) {
-    return;
+const moveUp = (knots: Coord[]): Coord[] => {
+  knots[0][1] += 1; // moving head
+
+  // follow up tails
+  for (let i = 1; i < knots.length; i++) {
+    if (isTouching(knots[i - 1], knots[i])) {
+      continue;
+    }
+    knots[i][1] += 1;
+    if (!isSameX(knots[i - 1], knots[i])) {
+      knots[i][0] = knots[i - 1][0];
+    }
   }
-  t[1] += 1;
-  if (!isSameX()) {
-    t[0] = h[0];
-  }
-  res.add(stringifyCoord(t));
+
+  return knots;
 };
 
-const moveDown = () => {
-  h[1] -= 1;
-  if (isTouching()) {
-    return;
+const moveDown = (knots: Coord[]): Coord[] => {
+  knots[0][1] -= 1; // moving head
+
+  // follow up tails
+  for (let i = 1; i < knots.length; i++) {
+    if (isTouching(knots[i - 1], knots[i])) {
+      continue;
+    }
+    knots[i][1] -= 1;
+    if (!isSameX(knots[i - 1], knots[i])) {
+      knots[i][0] = knots[i - 1][0];
+    }
   }
-  t[1] -= 1;
-  if (!isSameX()) {
-    t[0] = h[0];
-  }
-  res.add(stringifyCoord(t));
+
+  return knots;
 };
 
-const moveRight = () => {
-  h[0] += 1;
+const moveRight = (knots: Coord[]): Coord[] => {
+  knots[0][0] += 1; // moving head
 
-  if (isTouching()) {
-    return;
+  // follow up tails
+  for (let i = 1; i < knots.length; i++) {
+    if (isTouching(knots[i - 1], knots[i])) {
+      continue;
+    }
+    knots[i][0] += 1;
+    if (!isSameY(knots[i - 1], knots[i])) {
+      knots[i][1] = knots[i - 1][1];
+    }
   }
-  t[0] += 1;
-  if (!isSameY()) {
-    t[1] = h[1];
-  }
-  res.add(stringifyCoord(t));
+
+  return knots;
 };
 
-const moveLeft = () => {
-  h[0] -= 1;
-  if (isTouching()) {
-    return;
+const moveLeft = (knots: Coord[]): Coord[] => {
+  knots[0][0] -= 1; // moving head
+
+  // follow up tails
+  for (let i = 1; i < knots.length; i++) {
+    if (isTouching(knots[i - 1], knots[i])) {
+      continue;
+    }
+    knots[i][0] -= 1;
+    if (!isSameY(knots[i - 1], knots[i])) {
+      knots[i][1] = knots[i - 1][1];
+    }
   }
-  t[0] -= 1;
-  if (!isSameY()) {
-    t[1] = h[1];
-  }
-  res.add(stringifyCoord(t));
+
+  return knots;
 };
 
-inputs.forEach((input) => {
-  const [cmd, args] = input.split(' ');
-  const count = Number(args);
+const part1 = () => {
+  let knots: [Coord, Coord] = [
+    [0, 0],
+    [0, 0],
+  ];
 
-  let func:
-    | typeof moveUp
-    | typeof moveDown
-    | typeof moveLeft
-    | typeof moveRight;
+  const res = new Set<string>([stringifyCoord(knots[knots.length - 1])]);
 
-  switch (cmd) {
-    case 'R':
-      func = moveRight;
-      break;
-    case 'L':
-      func = moveLeft;
-      break;
-    case 'U':
-      func = moveUp;
-      break;
-    case 'D':
-      func = moveDown;
-      break;
-  }
+  inputs.forEach((input) => {
+    const [cmd, args] = input.split(' ');
+    const count = Number(args);
 
-  Array.from({ length: count }).forEach(() => func());
-});
+    let func:
+      | typeof moveUp
+      | typeof moveDown
+      | typeof moveLeft
+      | typeof moveRight;
 
-console.log(res.size);
+    switch (cmd) {
+      case 'R':
+        func = moveRight;
+        break;
+      case 'L':
+        func = moveLeft;
+        break;
+      case 'U':
+        func = moveUp;
+        break;
+      case 'D':
+        func = moveDown;
+        break;
+    }
+
+    Array.from({ length: count }).forEach(() => {
+      const newKnots = func(knots) as [Coord, Coord];
+      knots = newKnots;
+      res.add(stringifyCoord(knots[knots.length - 1]));
+    });
+  });
+
+  console.log(res.size);
+};
+
+part1();
